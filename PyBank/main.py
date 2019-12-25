f4f9367111ea00a59292b15fb302a1d2e3316fd3
Alec Slaney <alecg.sla@gmail.com>
2019-12-25 12:36:08 -0800

    Progress update

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,8 @@ import os
 import csv
 
 months = 0
+profitLoss = 0
+totalProfitLoss = 0
 
 #Path to data in csv file
 budgetDataPath = os.path.join("Resources", "budget_data.csv")
@@ -12,9 +14,12 @@ with open(budgetDataPath, newline="") as csvfile:
     #Store and skip header
     csvHeader = next(csvfile)
     for row in csvReader:
+        profitLoss = int(row[1])
+        totalProfitLoss = totalProfitLoss + profitLoss
         months += 1
 
 # Printing summary of data
 print("Financial Analysis")
 print("------------------")
 print(f"Total Months: {months}")
+print(f"Total: {totalProfitLoss}")
