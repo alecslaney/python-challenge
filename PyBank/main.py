8af5efec88b9fee70c17e4a02baa3c2d9f2c6de3
Alec Slaney <alecg.sla@gmail.com>
2019-12-25 17:38:54 -0800

    Progress update, functionality completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,13 +4,9 @@ import csv
 months = 0
 profitLoss = 0
 totalProfitLoss = 0
-greatestDecrease = 0
-greatestIncrease = 0
-greatestDecreaseDate = "blank"
-greatestIncreaseDate = "blank"
 dateList = []
 profitLossList = []
-changeAverage = []
+changeList = []
 
 # Calculates average for given input (list)
 def average(numbers):
@@ -22,31 +18,34 @@ def average(numbers):
 
 # Calculates value of change in profit/loss between months and stores in a list
 def averageChange(numbers, dates):
-    global greatestDecrease, greatestIncrease
     length = len(numbers)
+    maxDecrease = 0
+    maxIncrease = 0
+    decreaseDate = ""
+    increaseDate = ""
     i = 1
     while i < length:
         change = int(int(numbers[i]) - int(numbers[i - 1]))
-        changeAverage.append(change)
+        changeList.append(change)
         if change < 0:
-            if change < greatestDecrease:
-                #print(dates[i])
-                greatestDecrease = change
-                greatestDecreaseDate == dates[i]
+            if change < maxDecrease:
+                maxDecrease = change
+                decreaseDate = dates[i]
         elif change > 0:
-            if change > greatestIncrease:
-                greatestIncrease = change
-                greatestIncreaseDate == dates[i]
+            if change > maxIncrease:
+                maxIncrease = change
+                increaseDate = dates[i]
         i += 1
-        
+    print(f"Average Change: ${round(average(changeList), 2)}")
+    print(f"Greatest Increase in Profits: {increaseDate} (${maxIncrease})") 
+    print(f"Greatest Decrease in Profits: {decreaseDate} (${maxDecrease})")
+
 # Path to data in csv file
 budgetDataPath = os.path.join("Resources", "budget_data.csv")
 
 with open(budgetDataPath, newline="") as csvfile:
     csvReader = csv.reader(csvfile, delimiter=",")
-
-    # Store and skip header
-    csvHeader = next(csvfile)
+    next(csvfile)
     for row in csvReader:
         dateList.append(row[0])
         profitLossList.append(row[1])
@@ -54,14 +53,9 @@ with open(budgetDataPath, newline="") as csvfile:
         totalProfitLoss += profitLoss
         months += 1
 
-averageChange(profitLossList, dateList)
-#print(f"{dateList}")
-
 # Printing summary of data
 print("Financial Analysis")
 print("------------------")
 print(f"Total Months: {months}")
 print(f"Total: ${totalProfitLoss}")
-print(f"Average Change: ${round(average(changeAverage), 2)}")
-print(f"Greatest Increase in Profits: {greatestIncreaseDate} (${greatestIncrease})")
-print(f"Greatest Decrease in Profits: {greatestDecreaseDate} (${greatestDecrease})")
+averageChange(profitLossList, dateList)
